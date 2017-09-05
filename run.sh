20cb0ca57e58e8815e1e42ec310968c66fa27ccc
Nicolas Antoniazzi <nicolas.antoniazzi@gmail.com>
2017-09-05 19:25:32 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,8 +2,9 @@
 export DISPLAY=:99
 /usr/bin/Xvfb :99 -screen 0 1024x768x24 -ac +extension GLX +extension RANDR +render -noreset &
 x11vnc -ncache 10 -localhost -display :99 &
-xclock &
 cd noVNC/utils
 ./launch.sh --vnc localhost:5900 &
+fluxbox &
 echo "TECHIO> open -p 6080 /vnc-redirect.html"
-sleep 60
+echo "TECHIO> terminal -i 'export DISPLAY=:99;clear;gimp 2>/dev/null'"
+sleep 120
