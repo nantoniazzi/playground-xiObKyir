10b1ed99ddea35a2b60e1ec75a6b4192a17824c3
Nicolas Antoniazzi <nicolas.antoniazzi@gmail.com>
2017-09-05 18:22:16 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 apt update
-apt -y install xvfb x11vnc x11-apps net-tools git
-git clone https://github.com/novnc/noVNC.git
+apt-get -y install xvfb x11vnc x11-apps net-tools git
+git clone http://github.com/novnc/noVNC.git
