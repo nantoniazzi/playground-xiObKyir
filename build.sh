5090c9714355e428dcf74de7dab213d30a0c06d5
Nicolas Antoniazzi <nicolas.antoniazzi@gmail.com>
2017-09-05 18:58:39 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 export DEBIAN_FRONTED=noninteractive
 apt-get update
-apt-get -y install ca-certificates
+#apt-get -y install ca-certificates
 apt-get -y install xvfb x11vnc x11-apps net-tools git ca-certificates python apt-utils python-numpy
+git config --global http.sslverify false
 git clone http://github.com/novnc/noVNC.git
 mv vnc-redirect.html noVNC/
