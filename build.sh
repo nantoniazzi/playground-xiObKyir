ef82d9005477720f06b3eb5e6a6abea9c208df48
Nicolas Antoniazzi <nicolas.antoniazzi@gmail.com>
2017-09-05 18:50:05 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
+export DEBIAN_FRONTED=noninteractive
 apt-get update
 apt-get -y install xvfb x11vnc x11-apps net-tools git ca-certificates python apt-utils python-numpy
 git clone http://github.com/novnc/noVNC.git
