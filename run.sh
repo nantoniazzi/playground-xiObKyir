6fd7d0e317140ac109424d5a3b36c9bd5a7182d4
Nicolas Antoniazzi <nicolas.antoniazzi@gmail.com>
2017-09-05 18:41:51 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,9 @@
-echo "TECHIO> terminal"
+# echo "TECHIO> terminal"
+export DISPLAY=:99
+/usr/bin/Xvfb :99 -screen 0 1024x768x24 -ac +extension GLX +extension RANDR +render -noreset &
+x11vnc -ncache 10 -localhost -display :99 &
+xclock &
+cd noVNC/utils
+./launch.sh --vnc localhost:5900 &
+echo "TECHIO> open -p 6080 /vnc-redirect.html"
+sleep 60
