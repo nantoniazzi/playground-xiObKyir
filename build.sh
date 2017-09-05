a38159a478e7f4ea549965f3fe1aac1fc4521929
Nicolas Antoniazzi <nicolas.antoniazzi@gmail.com>
2017-09-05 18:46:34 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 apt-get update
-apt-get -y install xvfb x11vnc x11-apps net-tools git ca-certificates python
+apt-get -y install xvfb x11vnc x11-apps net-tools git ca-certificates python apt-utils python-numpy
 git clone http://github.com/novnc/noVNC.git
 mv vnc-redirect.html noVNC/
