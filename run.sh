ebd65a6ad0cfe752e9c7d9dc6569f1f7a921faa3
Nicolas Antoniazzi <nicolas.antoniazzi@gmail.com>
2017-09-06 12:20:19 +0200

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
-# echo "TECHIO> terminal"
 export DISPLAY=:99
-/usr/bin/Xvfb :99 -screen 0 1024x768x24 -ac +extension GLX +extension RANDR +render -noreset &
-x11vnc -ncache 10 -localhost -display :99 &
+
+/usr/bin/Xvfb :99 -screen 0 1024x600x24 -ac +extension GLX +extension RANDR +render -noreset &>/dev/null &
+x11vnc -shared -ncache 10 -localhost -display :99 &>/dev/null &
 cd noVNC/utils
-./launch.sh --vnc localhost:5900 &
-fluxbox &
-echo "TECHIO> open -p 6080 /vnc-redirect.html"
-echo "TECHIO> terminal -i 'export DISPLAY=:99;clear;gimp 2>/dev/null'"
-sleep 120
+./launch.sh --vnc localhost:5900 &>/dev/null &
+echo "session.screen0.workspaces:1" > ~/.fluxbox/init
+fluxbox &>/dev/null &
+
+echo "TECHIO> open -p 6080 /vnc_lite.html"
+echo "TECHIO> terminal -i 'export DISPLAY=:99;clear; sleep 1; gimp &>/dev/null &'"
+sleep 180
