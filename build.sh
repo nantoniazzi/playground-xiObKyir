ed449e6234fd355439cffceae428230bc51ddd98
Nicolas Antoniazzi <nicolas.antoniazzi@gmail.com>
2017-09-06 15:46:02 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,3 +5,8 @@ apt-get -y install xvfb x11vnc x11-apps net-tools git ca-certificates python apt
 git config --global http.sslverify false
 git clone http://github.com/novnc/noVNC.git
 mv vnc-redirect.html noVNC/
+/usr/bin/Xvfb :99 -screen 0 1024x550x24 -ac +extension GLX +extension RANDR +render -noreset &>/dev/null &
+fluxbox &>/dev/null &
+sleep 2
+killall -9 fluxbox
+echo "session.screen0.workspaces:1" > /root/.fluxbox/init
