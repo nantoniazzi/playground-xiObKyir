be804ec80feb238851f1fccb720855cdbfb97b5e
Nicolas Antoniazzi <nicolas.antoniazzi@gmail.com>
2017-09-06 16:22:18 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ git config --global http.sslverify false
 git clone http://github.com/novnc/noVNC.git
 mv vnc-redirect.html noVNC/
 /usr/bin/Xvfb :99 -screen 0 1024x550x24 -ac +extension GLX +extension RANDR +render -noreset &>/dev/null &
-fluxbox &>/dev/null &
+/usr/bin/fluxbox &>/dev/null &
 sleep 2
 killall -9 fluxbox
-echo "session.screen0.workspaces:1" > /root/.fluxbox/init
+echo "session.screen0.workspaces:1" >> /root/.fluxbox/init
